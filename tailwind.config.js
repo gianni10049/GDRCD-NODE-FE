547c37beadf43dbc23e555a8db76fa72c58c0afa
gianni10049 <giovanni.pane10049@gmail.com>
2022-06-18 00:20:27 +0200

    GDRCD_NODE [FIX]     - Ripristinato parzialmente tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,25 @@ module.exports = {
 				'TecFont': 'TecFont',
 			},
 			colors: {
-				//TODO SOSTITUIRE E RIMUOVERE CON CHAKRA
 				'green': {
+					background: 'rgba(6,52,38,0.73)',
 					border: 'rgba(16,164,120,0.73)',
+					text: 'rgba(6,52,38,1)',
+					textLight: 'rgba(193,215,199,1)',
 					light: 'rgb(17,203,148)',
+					lightOpacity: 'rgba(17,203,148,0.3)',
+					backdrop: 'rgba(53, 203, 0, 0.5)',
+					backgroundDark: '#2f3e46',
+				},
+				'yellow': {
+					bg: 'gold',
+					text: '#998600',
+				},
+				'popover': {
+					info: 'rgba(255,255,255,0.7)',
+				},
+				'red': {
+					opacity: 'rgba(143,59,59,0.5)',
 				},
 			},
 		},
